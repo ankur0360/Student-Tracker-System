59a62f546534a32412aa37d067ac7429bfd4ddfc
Ankur Roy <94541480+ankur0360@users.noreply.github.com>
2023-11-23 18:54:29 +0530

    Update

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class UserProfile(models.Model):
+    username = models.CharField(unique=True,max_length=50)
+    password = models.CharField(max_length=50)
+    def __str__(self):
+      return self.username
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                return render(request, 'login.html', {'form': form, 'error': 'Invalid login credentials'})
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def home(request):
+    return render(request, 'home.html')
